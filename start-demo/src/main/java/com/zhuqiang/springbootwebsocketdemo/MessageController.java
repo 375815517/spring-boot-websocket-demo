7fe0fd8be643020b11c33ec630b3bb3de045ac06
375815517@qq.com <375815517@qq.com>
2018-12-07 14:38:53 +0800

    1.模块拆分

--- start-demo/src/main/java/com/zhuqiang/springbootwebsocketdemo/MessageController.java
+++ start-demo/src/main/java/com/zhuqiang/springbootwebsocketdemo/MessageController.java
@@ -3,7 +3,6 @@ package com.zhuqiang.springbootwebsocketdemo;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.util.HtmlUtils;
 
 /**
  * websocket message controller
